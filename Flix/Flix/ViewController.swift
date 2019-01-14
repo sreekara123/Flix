14e4cc31f6d27f72bc25d7909a059dba12d3eed1
Sreekara <syachama@purdue.edu>
2019-01-14 18:28:59 -0500

    xcode initialization

---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Sreekara Yachamaneni on 1/14/19.
+//  Copyright © 2019 Sreekara Yachamaneni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
